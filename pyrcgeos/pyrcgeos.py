bb6e590b016b662421ad90e46651e731e8ef21ad
rcgeos <renato.cumani4@gmail.com>
2023-04-06 09:49:55 -0400

    added docString

--- pyrcgeos/pyrcgeos.py
+++ pyrcgeos/pyrcgeos.py
@@ -4,6 +4,17 @@ import random
 import string
 
 def generate_random_string(length=10, upper=False, digits=False, punctuation=False):
+    """Generate a random string of a given length
+
+    Args:
+        length (int, optional): _description_. Defaults to 10.
+        upper (bool, optional): _description_. Defaults to False.
+        digits (bool, optional): _description_. Defaults to False.
+        punctuation (bool, optional): _description_. Defaults to False.
+
+    Returns:
+        _type_: The generated string
+    """    
     letters = string.ascii_lowercase
     if upper:
         letters += string.ascii_lowercase
@@ -15,5 +26,13 @@ def generate_random_string(length=10, upper=False, digits=False, punctuation=Fal
     return ''.join(random.choice(letters) for i in range(length))
 
 def generate_lucky_number(length=1):
+    """Generate a random string of a given length
+
+    Args:
+        length (int, optional): _description_. Defaults to 1.
+
+    Returns:
+        _type_: Digits of a defined length
+    """    
     result_str = ''.join(random.choice(string.digits) for i in range(length))
     return int(result_str)
