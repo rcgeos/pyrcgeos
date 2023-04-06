52cdcc2728d9669fa6b05eea5719462b67643db2
rcgeos <renato.cumani4@gmail.com>
2023-04-06 16:00:47 -0400

    add python functions

--- pyrcgeos/pyrcgeos.py
+++ pyrcgeos/pyrcgeos.py
@@ -2,6 +2,75 @@
 
 import random
 import string
+import ipyleaflet
+
+class Map(ipyleaflet.Map):
+    """Map class to add Map, basemap, layers and controls
+
+    Args:
+        ipyleaflet (_type_): _description_
+    """    
+    def __init__(self, center, zoom, **kwargs) -> None:
+        """Add center (Lat, Lon), zoom
+
+        Args:
+            center (_type_): _description_
+            zoom (_type_): _description_
+        """        
+        if "scroll_wheel_zoom" not in kwargs:
+            kwargs["scroll_wheel_zoom"]=True
+        super().__init__(center=center, zoom=zoom, **kwargs)
+
+    def add_search_control(self, position="topleft",**kwargs):
+        """Add Search Control button 
+        """        
+        if "url" not in kwargs:
+            kwargs['url'] = 'https://nominatim.openstreetmap.org/search?format=json&q={s}'
+      
+        search_control = ipyleaflet.SearchControl(position=position, **kwargs)
+        self.add_control(search_control)
+
+    def add_draw_control(self,**kwargs):
+        """Adds a draw control to the map.
+        Args: 
+            kwargs: Keyword arguments to pass to the draw control
+        """        
+        draw_control = ipyleaflet.DrawControl(**kwargs)
+        draw_control.polyline =  {
+            "shapeOptions": {
+                "color": "#6bc2e5",
+                "weight": 8,
+                "opacity": 1.0
+            }
+        }
+        draw_control.polygon = {
+            "shapeOptions": {
+                "fillColor": "#6be5c3",
+                "color": "#6be5c3",
+                "fillOpacity": 1.0
+            },
+            "drawError": {
+                "color": "#dd253b",
+                "message": "Oups!"
+            },
+            "allowIntersection": False
+        }
+        draw_control.circle = {
+            "shapeOptions": {
+                "fillColor": "#efed69",
+                "color": "#efed69",
+                "fillOpacity": 1.0
+            }
+        }
+        draw_control.rectangle = {
+            "shapeOptions": {
+                "fillColor": "#fca45d",
+                "color": "#fca45d",
+                "fillOpacity": 1.0
+            }
+        }
+        self.add_control(draw_control)
+
 
 def generate_random_string(length=10, upper=False, digits=False, punctuation=False):
     """Generate a random string of a given length
