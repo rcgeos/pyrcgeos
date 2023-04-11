2ea3796e347432f7d9178f550d5c43774f78eefd
rcgeos <renato.cumani4@gmail.com>
2023-04-11 09:20:14 -0400

    add examples demo

--- pyrcgeos/pyrcgeos.py
+++ pyrcgeos/pyrcgeos.py
@@ -8,18 +8,34 @@ class Map(ipyleaflet.Map):
     """Map class to add Map, basemap, layers and controls
 
     Args:
-        ipyleaflet (_type_): _description_
+        ipyleaflet (ipyleaflet.Map): An ipyleaflet map.
     """    
-    def __init__(self, center, zoom, **kwargs) -> None:
+    def __init__(self, **kwargs) -> None:
         """Add center (Lat, Lon), zoom
 
         Args:
             center (_type_): _description_
             zoom (_type_): _description_
-        """        
+        """   
+        if "center" not in kwargs:
+            kwargs["center"] = [40, -100]
+
+        if "zoom" not in kwargs:
+            kwargs["zoom"] = 4        
         if "scroll_wheel_zoom" not in kwargs:
             kwargs["scroll_wheel_zoom"]=True
-        super().__init__(center=center, zoom=zoom, **kwargs)
+        super().__init__(**kwargs)
+
+        if "layers_control" not in kwargs:
+            kwargs["layers_control"] = True
+        if "layers_control":
+            self.add_layers_control()
+        if "fullscreen_control" not in kwargs:
+            kwargs["fullscreen_control"] = True
+        if "fullscreen_control":
+            self.add_fullscreen_control(position="topleft")
+        self.add_draw_control()
+        self.add_search_control(position="topleft")
 
     def add_search_control(self, position="topleft",**kwargs):
         """Add Search Control button 
@@ -70,6 +86,39 @@ class Map(ipyleaflet.Map):
             }
         }
         self.add_control(draw_control)
+
+    def add_layers_control(self, position="topright", **kwargs):
+        """Add a layers control to the map. 
+        Args: 
+            kwargs: Keyword arguments to pass to the layers control
+        """        
+        layers_control = ipyleaflet.LayersControl(position=position,**kwargs)
+        self.add_control(layers_control)
+
+    def add_fullscreen_control(self, position="topleft"):
+        """Add a fullscreen control to the map
+
+        Args:
+            position (str, optional): _description_. Defaults to "topleft".
+        """        
+        fullscreen_control=ipyleaflet.FullScreenControl(position=position)
+        self.add_control(fullscreen_control)
+
+    def add_tile_layer(self, url, name, attribution,**kwargs):
+        """Adds a tile layer to the map.
+
+        Args:
+            url (_type_): _description_
+            name (_type_): _description_
+            attribution (_type_): _description_
+        """        
+        tile_layer = ipyleaflet.TileLayer(
+            url=url,
+            name=name,
+            attribution=attribution,
+            **kwargs
+        )
+        self.add_layer(tile_layer)
 
 
 def generate_random_string(length=10, upper=False, digits=False, punctuation=False):
